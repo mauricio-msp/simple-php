f8e78ce4c102980ea968e16579f47723e7597779
Maurício Porfírio <mauriciosporfirio@gmail.com>
2017-06-06 00:28:49 -0300

    Update HomeController.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,7 +7,6 @@ class HomeController extends \Src\Http\Controller {
     
     public function view() {
         echo $this->service->render('template/welcome.twig', [
-            'nome'  => $this->request->name,
             'title' => 'simple-php',
             'data'  => date('Y'),
             'asset' => url('/public')
